f5e44ed1505bed15df8c51471108dbec17d1a72b
Marc Parsons <maparson@ualberta.ca>
2020-06-04 17:12:13 -0600

    Removed obsolete packages from packagelist.

--- Scripts/Packages.R
+++ Scripts/Packages.R
@@ -1,7 +1,5 @@
 #--------------------------------------#
 # Packages file
-# Version 2
-# 19/04/2020
 #--------------------------------------#
 
 # Get the list of packages.
@@ -18,54 +16,6 @@ packagelist <- c(
   "shiny",
   "stats",
   "stars",
-  "spatial.tools",
-  "readr",
-  "rmapshaper",
-  "geogrid",
-  "rlist"
-)
-
-# Get packages function.
-
-get.packages <- function(plist) {
-  
-  #oldw <- getOption("warn") # Suppress annoying warnings.
-  #options(warn = -1)
-  
-  for(i in 1:length(plist)) {
-    name <- as.character(plist[i])
-    if(!(name %in% installed.packages())) {
-      install.packages(name)
-      require(name, character.only=TRUE)
-    } else {
-      require(name, character.only=TRUE)
-    }
-  }
-  
-  #options(warn = oldw) # Turn the warnings back on.
-  
-}
-#--------------------------------------#
-# Packages file
-# Version 2
-# 19/04/2020
-#--------------------------------------#
-
-# Get the list of packages.
-
-packagelist <- c(
-  "dplyr",
-  "tidyverse",
-  "ggplot2",
-  "MASS",
-  "scales",
-  "sf",
-  "raster",
-  "lubridate",
-  "shiny",
-  "stats",
-  "stars",
-  "spatial.tools",
   "readr",
   "rmapshaper",
   "geogrid",
