7277998a83673b3bb8101223b02d16c84065f4de
Marc Parsons <maparson@ualberta.ca>
2020-06-05 11:13:04 -0600

    Added in Edmonton data.

--- Scripts/Grids-province.R
+++ Scripts/Grids-province.R
@@ -6,8 +6,40 @@
 
 index_ab <- tribble(
   ~x, ~y, ~R,  ~FED,
-   2,  1, "S", "Medicine Hat - Cardston - Warner",
-   1,  1, "S", "Foothills",
-   2,  2, "S", "Lethbridge",
-   3,  1, "S", "Bow River"
+  
+  2,  1,  "So",  "Medicine Hat - Cardston - Warner",
+  1,  1,  "So",  "Foothills",
+  2,  2,  "So",  "Lethbridge",
+  3,  1,  "So",  "Bow River",
+   
+  1,  3,  "Ca",  "Calgary - Heritage",
+  2,  3,  "Ca",  "Calgary - Midnapore",
+  3,  2,  "Ca",  "Calgary - Shepard",
+   
 )
+
+index_ed <- tribble(
+   ~x, ~y, ~R,  ~PARTY,  ~FED,
+   
+   1,  1,  "Ed",  "CON",  "Edmonton Riverbend",  
+   1,  2,  "Ed",  "CON",  "Edmonton West",
+   2,  1,  "Ed",  "CON",  "Edmonton - Wetaskiwin",
+   2,  2,  "Ed",  "NDP",  "Edmonton Strathcona",
+   3,  1,  "Ed",  "CON",  "Edmonton Millwoods",
+   3,  2,  "Ed",  "CON",  "Sherwood Park - Fort Saskatchewan",
+   2,  3,  "Ed",  "CON",  "Edmonton Centre",
+   1,  3,  "Ed",  "CON",  "St. Albert - Edmonton",
+   2,  4,  "Ed",  "CON",  "Edmonton Griesbach",
+   3,  3,  "Ed",  "CON",  "Edmonton Manning"
+   
+)
+
+
+
+
+
+
+
+
+
+
