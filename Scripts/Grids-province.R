df33af9a59179aa9b512fb81c8bb183d178941a5
Marc Parsons <maparson@ualberta.ca>
2020-06-04 18:11:31 -0600

    Added a provincial grid file. Added S Alberta.

---
+++ Scripts/Grids-province.R
@@ -0,0 +1,13 @@
+#----------------------------#
+# Province by province grids #
+#----------------------------#
+
+# Alberta
+
+index_ab <- tribble(
+  ~x, ~y, ~R,  ~FED,
+   2,  1, "S", "Medicine Hat - Cardston - Warner",
+   1,  1, "S", "Foothills",
+   2,  2, "S", "Lethbridge",
+   3,  1, "S", "Bow River"
+)
